1985e3d7d3d6203147843d903eada57bf012aad4
giantkuo <toolmenshare@gmail.com>
2023-11-22 09:38:31 +0000

    Updated auto_check.py to v5. Incoorperate TaskFlow API.
    Switch to using TaskFlow API to deal with data passing betwen tasks.

--- auto_check.py
+++ auto_check.py
@@ -1,94 +1,71 @@
 import os
 import smtplib
 import pendulum
-from datetime import datetime, timedelta
 from email.message import EmailMessage
-from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
+from airflow.decorators import dag, task
 
 local_tz = pendulum.timezone("Asia/Taipei")
-
-today_date = datetime.today().date().strftime("%Y-%m-%d")
 base_folder = "/mnt/nas-data/Animal/pig_video/andrew_test/"
 hours_to_check = ["08", "09", "10", "11", "12", "13", "14", "15", "16", "17"]
 recipient_email = "r12631026@ntu.edu.tw"
-alert_message_body = "\n{} Auto Check Results:\n\n".format(today_date)
-
-default_args = {
-    "owner": "Andrew Hsieh",
-    "depends_on_past": False,
-    "retries": 1,
-    "retry_delay": timedelta(minutes=1),
-}
 
 
-def check_folder(folder):
-    try:
-        if not os.listdir(folder):
-            alert("Empty folder", folder)
-        else:
-            alert("Good", folder)
-    except FileNotFoundError:
-        alert("File not found", folder)
+@dag(
+    dag_id="auto_check",
+    owner="Andrew Hsieh",
+    start_date=pendulum.datetime(2021, 1, 1, tz=local_tz),
+    schedule="@daily",
+    catchup=False,
+    retries=1,
+)
+def auto_check_flow():
+    def check_folder(folder):
+        try:
+            file = os.listdir(folder)
+            if not file:
+                return "[{}] Empty folder.\n".format(folder[-8:-1])
+            else:
+                return "[{}] Good. {} files found.\n".format(folder[-8:-1], len(file))
+        except FileNotFoundError:
+            return "[{}] File not found.\n".format(folder[-8:-1])
 
+    @task
+    def check_all_folders():
+        today = pendulum.today(local_tz).format("%Y-%m-%d")
+        status_log = "\n{} Auto Check Results:\n\n".format(today)
+        for hour in hours_to_check:
+            status_log += check_folder(base_folder + "rpi_2/{}/{}/".format(today, hour))
+            status_log += check_folder(base_folder + "rpi_3/{}/{}/".format(today, hour))
+        return status_log
 
-def send_email(subject="Test", body="Test", to_email=recipient_email):
-    # Sender details
-    sender_email = "dummydumdum375@gmail.com"
-    sender_password = "wlqx epfd hqlc ywnu"
+    @task
+    def email(subject, body, to_email):
+        sender_email = "dummydumdum375@gmail.com"
+        sender_password = "wlqx epfd hqlc ywnu"
 
-    # Set up the email
-    msg = EmailMessage()
-    msg.set_content(body)
-    msg["Subject"] = subject
-    msg["From"] = sender_email
-    msg["To"] = to_email
+        # Set up the email
+        msg = EmailMessage()
+        msg.set_content(body)
+        msg["Subject"] = subject
+        msg["From"] = sender_email
+        msg["To"] = to_email
 
-    # Connect to Gmail's SMTP server
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-        smtp.login(sender_email, sender_password)
-        smtp.send_message(msg)
+        # Connect to Gmail's SMTP server
+        try:
+            with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+                smtp.login(sender_email, sender_password)
+                smtp.send_message(msg)
+            print("Email sent to {}".format(to_email))
+        except Exception as e:
+            print("Failed to send email: {}".format(e))
 
-    print("Email sent to {}".format(to_email))
-
-
-def alert(error, folder):
-    global alert_message_body
-    folder = folder[39:]  # remove base_folder
-    if error == "Empty folder":
-        alert_message_body += "[{}] Empty folder. Camera may be down.\n".format(folder)
-    elif error == "File not found":
-        alert_message_body += (
-            "[{}] File not found. Internet connection may be down.\n".format(folder)
-        )
-    elif error == "Good":
-        alert_message_body += "[{}] Good.\n".format(folder)
-
-
-def check():
-    # check folder from 8am to 17pm for rpi2 and rpi3
-    for hour in hours_to_check:
-        check_folder(base_folder + "rpi_2/{}/{}/".format(today_date, hour))
-        check_folder(base_folder + "rpi_3/{}/{}/".format(today_date, hour))
-
-
-def email():
-    send_email(
-        subject="[{}] Rpi camera system auto check".format(today_date),
-        body=alert_message_body,
+    email(
+        subject="[{}] Rpi camera system auto check".format(
+            pendulum.today(local_tz).format("%Y-%m-%d")
+        ),
+        body=check_all_folders(),
+        to_email=recipient_email,
     )
 
 
-with DAG(
-    dag_id="auto_check",
-    default_args=default_args,
-    schedule_interval="0 19 * * *",
-    start_date=datetime(2021, 1, 1, tzinfo=local_tz),
-    catchup=False,
-    max_active_runs=1,
-) as dag:
-    check_task = PythonOperator(task_id="check", python_callable=check)
-
-    email_task = PythonOperator(task_id="send_email", python_callable=email)
-
-    check_task >> email_task
+auto_check_flow()
