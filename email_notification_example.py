7492729e0a48fea4ff680b29662549ce4e11cc1f
giantkuo <toolmenshare@gmail.com>
2024-02-22 14:53:34 +0000

    email_notification v0.6.5

--- email_notification_example.py
+++ email_notification_example.py
@@ -40,10 +40,13 @@ def check_sftp_file():
     sftp = ssh.open_sftp()
     global result
     try:
-        sftp.stat(remote_path)
-        result = f"File or directory exists: {remote_path}"
+        files = sftp.listdir(remote_path)
+        if files:
+            result = "[{}] Good. {} files found.\n".format(remote_path[-16:-1], len(files))
+        else:
+            result = "[{}] Empty folder.\n".format(remote_path[-16:-1])
     except FileNotFoundError:
-        result = f"File or directory does not exist: {remote_path}"
+        result = "[{}] File not found.\n".format(folder[-16:-1])
 
     # close connection
     sftp.close()
@@ -129,60 +132,3 @@ with DAG(
     # pwd_task >> check_all_folders_task >> email_task
     check_file_task >> email_task
 
-'''
-@dag(
-    dag_id="email_notification_example",
-    default_args=default_args,
-)
-def auto_check_flow():
-    def check_folder(folder):
-        try:
-            file = os.listdir(folder)
-            if not file:
-                return "[{}] Empty folder.\n".format(folder[-8:-1])
-            else:
-                return "[{}] Good. {} files found.\n".format(folder[-8:-1], len(file))
-        except FileNotFoundError:
-            return "[{}] File not found.\n".format(folder[-8:-1])
-
-    @task(task_id="check_all_folders")
-    def check_all_folders():
-        today = pendulum.today(local_tz).format("%Y-%m-%d")
-        status_log = "\n{} Auto Check Results:\n\n".format(today)
-        for hour in hours_to_check:
-            status_log += check_folder(base_folder + "rpi_2/{}/{}/".format(today, hour))
-            status_log += check_folder(base_folder + "rpi_3/{}/{}/".format(today, hour))
-        return status_log
-
-    @task(task_id="email")
-    def email(subject, body, to_email):
-        sender_email = "dummydumdum375@gmail.com"
-        sender_password = "wlqx epfd hqlc ywnu"
-
-        # Set up the email
-        msg = EmailMessage()
-        msg.set_content(body)
-        msg["Subject"] = subject
-        msg["From"] = sender_email
-        msg["To"] = to_email
-
-        # Connect to Gmail's SMTP server
-        try:
-            with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-                smtp.login(sender_email, sender_password)
-                smtp.send_message(msg)
-            print("Email sent to {}".format(to_email))
-        except Exception as e:
-            print("Failed to send email: {}".format(e))
-
-    email(
-        subject="[{}] Rpi camera system auto check".format(
-            pendulum.today(local_tz).format("%Y-%m-%d")
-        ),
-        body=check_all_folders(),
-        to_email=recipient_email,
-    )
-
-
-auto_check_flow()
-'''
